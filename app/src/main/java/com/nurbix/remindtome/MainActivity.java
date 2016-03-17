323b0a50282cd17429d7b904ad1a6ee97130d515
nurbix <nurbeka@gmail.com>
2016-03-16 22:53:36 -0500

    implemented navigation view and tab navigation with example fragment

--- app/src/main/java/com/nurbix/remindtome/MainActivity.java
+++ app/src/main/java/com/nurbix/remindtome/MainActivity.java
@@ -1,10 +1,16 @@
 package com.nurbix.remindtome;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.nurbix.remindtome.adapter.TabsPagerFragmentAdapter;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
-    DrawerLayout drawerLayout;
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         
         initToolbar();
         initNavigationView();
+        initTabs();
+
     }
 
     private void initToolbar() {
@@ -37,7 +47,40 @@ public class MainActivity extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.menu);
     }
 
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                            drawerLayout, toolbar, R.string.view_nav_open, R.string.view_nav_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.action_notification_item:
+                        showNotificationTab();
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
