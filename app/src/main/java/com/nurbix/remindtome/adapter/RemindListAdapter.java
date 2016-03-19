7ed812f3d211324a011f07f7b129565d9f916015
nurbix <nurbeka@gmail.com>
2016-03-18 21:32:01 -0500

    card view added

---
+++ app/src/main/java/com/nurbix/remindtome/adapter/RemindListAdapter.java
@@ -0,0 +1,50 @@
+package com.nurbix.remindtome.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.nurbix.remindtome.R;
+import com.nurbix.remindtome.dto.RemindDTO;
+
+import java.util.List;
+
+public class RemindListAdapter extends RecyclerView.Adapter<RemindListAdapter.RemindViewHolder> {
+
+    List<RemindDTO> data;
+
+    public RemindListAdapter(List<RemindDTO> data) {
+        this.data = data;
+    }
+
+    @Override
+    public RemindViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.remind_item, parent, false);
+        return new RemindViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RemindViewHolder holder, int position) {
+        RemindDTO item = data.get(position);
+        holder.title.setText(item.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class RemindViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+        TextView title;
+        public RemindViewHolder(View itemView) {
+            super(itemView);
+
+            cardView = (CardView) itemView.findViewById(R.id.cardView);
+            title = (TextView) itemView.findViewById(R.id.remind_item_title);
+        }
+    }
+}
--- app/src/main/java/com/nurbix/remindtome/fragments/HistoryFragment.java
+++ app/src/main/java/com/nurbix/remindtome/fragments/HistoryFragment.java
@@ -3,15 +3,22 @@ package com.nurbix.remindtome.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nurbix.remindtome.R;
+import com.nurbix.remindtome.adapter.RemindListAdapter;
+import com.nurbix.remindtome.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends AbstractTabFragment {
 
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_history;
 
 
 
@@ -31,7 +38,22 @@ public class HistoryFragment extends AbstractTabFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.recycle_view);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(CreateMockRemindListData()));
+
         return view;
+    }
+
+    private List<RemindDTO> CreateMockRemindListData() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        return data;
     }
 
 }
